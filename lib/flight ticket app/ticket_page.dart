3b26236e91d718397b1f2de830bcedf6c93b3d14
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2021-09-13 15:36:29 +0630

    flight ticket ui second commit

--- lib/flight ticket app/ticket_page.dart
+++ lib/flight ticket app/ticket_page.dart
@@ -1,5 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class Seat {
+  final bool? isAvailable;
+  Seat({this.isAvailable});
+}
 
 class TicketPage extends StatefulWidget {
   const TicketPage({Key? key}) : super(key: key);
@@ -9,6 +16,129 @@ class TicketPage extends StatefulWidget {
 }
 
 class _TicketPageState extends State<TicketPage> {
+  Color appColor = Color(0xff3959b4);
+  late List<Seat> _item;
+  int _row = 10;
+  int _column = 14;
+  @override
+  void initState() {
+    Random random = Random();
+    _item = List<Seat>.generate(
+        10, (index) => Seat(isAvailable: random.nextBool()));
+    _item.forEach((element) {
+      print(element.isAvailable);
+    });
+    var _tmp = List.generate(
+        _column,
+        (index) => List<Seat>.generate(
+            _row, (index) => Seat(isAvailable: random.nextBool())));
+    _tmp.forEach((element) {
+      element.forEach((e) {
+        print(e.isAvailable);
+      });
+    });
+    super.initState();
+  }
+
+  List<Widget> _buildSeatTile() {
+    return [
+      Expanded(child: Container()),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'A',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'B',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'C',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(child: Container()),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'D',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'E',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'F',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'G',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(child: Container()),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'H',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'I',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        child: Center(
+          child: Text(
+            'J',
+            style: GoogleFonts.montserrat(fontSize: 12, color: Colors.white),
+          ),
+        ),
+      )),
+      Expanded(child: Container())
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,8 +168,7 @@ class _TicketPageState extends State<TicketPage> {
                 bottom: 0,
                 child: Container(
                   decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      borderRadius: BorderRadius.circular(24)),
+                      color: appColor, borderRadius: BorderRadius.circular(24)),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -56,7 +185,16 @@ class _TicketPageState extends State<TicketPage> {
                           ),
                         ),
                       ),
-                      Expanded(flex: 28, child: Placeholder()),
+                      Expanded(
+                          flex: 28,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                  flex: 1,
+                                  child: Row(children: _buildSeatTile())),
+                              Expanded(flex: 15, child: Placeholder())
+                            ],
+                          )),
                       Expanded(
                           flex: 4,
                           child: Column(
@@ -130,7 +268,7 @@ class _TicketPageState extends State<TicketPage> {
                                     alignment: Alignment.centerRight,
                                     child: MaterialButton(
                                         elevation: 8,
-                                        color: Colors.indigo,
+                                        color: appColor,
                                         onPressed: () {},
                                         child: Text(
                                           'Confirm',
@@ -153,13 +291,26 @@ class _TicketPageState extends State<TicketPage> {
                 left: 0,
                 child: Container(
                   height: 64,
+                  padding: EdgeInsets.symmetric(horizontal: 24),
                   decoration: BoxDecoration(
                       color: Colors.grey[50],
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(24),
                           topRight: Radius.circular(24))),
                   child: Row(
-                    children: [],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+                      IconButton(
+                          onPressed: () {}, icon: Icon(Icons.card_giftcard)),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.style),
+                        color: Colors.indigoAccent,
+                      ),
+                      IconButton(
+                          onPressed: () {}, icon: Icon(Icons.person_outline))
+                    ],
                   ),
                 ))
           ],
