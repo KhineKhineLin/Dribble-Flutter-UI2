b52e1531d709488588329477d510d85215227a33
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2021-09-15 11:07:51 +0630

    bookstore ui second commit

--- lib/bookstore app/book_store_main_page.dart
+++ lib/bookstore app/book_store_main_page.dart
@@ -16,10 +16,76 @@ class _BookStoreMainPageState extends State<BookStoreMainPage> {
       bottomNavigationBar: BottomAppBar(
         elevation: 16,
         child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24),
           decoration: BoxDecoration(
               color: Colors.grey[200],
               border: Border(top: BorderSide(color: Colors.grey))),
           height: 72,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.home,
+                    size: 30,
+                  ),
+                  Text(
+                    'Home',
+                    style: TextStyle(fontSize: 12),
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.book,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    'Bookmark',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.favorite,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    'Favorite',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.add_shopping_cart,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    'My Cart',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
       body: SingleChildScrollView(
@@ -28,8 +94,32 @@ class _BookStoreMainPageState extends State<BookStoreMainPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
                 height: deviceHeight / 9,
-                decoration: BoxDecoration(color: Colors.red),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.bookmark_border,
+                      size: 40,
+                      color: Colors.red,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        'Biblic Globe',
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24),
+                      ),
+                    ),
+                    Spacer(),
+                    Icon(
+                      Icons.more_horiz,
+                      size: 40,
+                    )
+                  ],
+                ),
               ),
               Container(
                 height: deviceHeight / 11,
@@ -46,22 +136,16 @@ class _BookStoreMainPageState extends State<BookStoreMainPage> {
                 decoration: BoxDecoration(color: Colors.green),
               ),
               Container(
-                height: deviceHeight/3,
-                decoration: BoxDecoration(
-                  color: Colors.blue
-                ),
+                height: deviceHeight / 3,
+                decoration: BoxDecoration(color: Colors.blue),
               ),
               Container(
-                height: deviceHeight/11,
-                decoration: BoxDecoration(
-                  color: Colors.green
-                ),
+                height: deviceHeight / 11,
+                decoration: BoxDecoration(color: Colors.green),
               ),
               Container(
-                height: deviceHeight/3,
-                decoration: BoxDecoration(
-                  color: Colors.blue
-                ),
+                height: deviceHeight / 3,
+                decoration: BoxDecoration(color: Colors.blue),
               )
             ],
           ),
