2837b6ce9b73e572ca445a6793023f0f91b371e1
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2021-10-19 22:58:28 +0630

    delete dog sitters app

--- lib/material_app.dart
+++ lib/material_app.dart
@@ -1,19 +1,15 @@
-import 'package:dribble_flutter_ui2/bookstore%20app/book_store_main_page.dart';
-
-import 'package:dribble_flutter_ui2/discount%20tour%20app/main_page.dart';
-import 'package:dribble_flutter_ui2/dog%20sitters%20app/dog_sitters.dart';
 import 'package:dribble_flutter_ui2/flight%20ticket%20app/ticket_page.dart';
 
 import 'package:flutter/material.dart';
 
 class Apps extends StatelessWidget {
-  const Apps({ Key? key }) : super(key: key);
+  const Apps({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DogSitters(),
+      home: TicketPage(),
     );
   }
 }
